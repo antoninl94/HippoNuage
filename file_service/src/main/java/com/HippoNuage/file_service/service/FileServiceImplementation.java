1c1f5d551cff7c594bf33e4c532577f6aedd3660
Noons-ledev <noah.briet@icloud.com>
2025-06-30 20:40:46 +0200

    Upload vers s3 gérée pour les types de fichiers acceptés

--- file_service/src/main/java/com/HippoNuage/file_service/service/FileServiceImplementation.java
+++ file_service/src/main/java/com/HippoNuage/file_service/service/FileServiceImplementation.java
@@ -7,12 +7,19 @@ import java.nio.file.Paths;
 import java.util.UUID;
 
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.HippoNuage.file_service.dto.UploadDto;
 import com.HippoNuage.file_service.model.File;
 import com.HippoNuage.file_service.repository.FileRepository;
+
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
 
 @Service
 public class FileServiceImplementation implements FileFacade {
@@ -21,11 +28,17 @@ public class FileServiceImplementation implements FileFacade {
 
     private final ValidateFileService validateFileService;
 
+    @Value("${aws.bucket}")
+    private String bucketName;
+
+    private final S3Client s3Client;
+
     private final Path rootLocation = Paths.get("upload");
 
-    public FileServiceImplementation(FileRepository fileRepository, ValidateFileService validateFileService) {
+    public FileServiceImplementation(FileRepository fileRepository, ValidateFileService validateFileService, S3Client s3Client) {
         this.fileRepository = fileRepository;
         this.validateFileService = validateFileService;
+        this.s3Client = s3Client;
     }
 
     @Override
@@ -37,57 +50,111 @@ public class FileServiceImplementation implements FileFacade {
         validateFileService.validateFile(multipartFile);
         System.out.println("validation ok : " + multipartFile.getOriginalFilename());
 
-        // Crée le dossier utilisateur s'il n'existe pas
-        Path userFolder = rootLocation.resolve(userId.toString());
-        Files.createDirectories(userFolder);
-
         String originalFileName = uploadDto.getName();
 
         String baseName = FilenameUtils.getBaseName(originalFileName);
         String extension = FilenameUtils.getExtension(originalFileName);
 
-        // Défini le chemin du fichier
-        Path destinationFile = userFolder.resolve(originalFileName)
-                .normalize()
-                .toAbsolutePath();
+        // Crée la clé s3 liée à l'utilisateur
+        String s3Key = userId + "/" + originalFileName;
+        Path pathS3Key = Paths.get(s3Key);
+        Path destinationFile = pathS3Key;
 
         // Renomme le fichier en cas de doublon
         int count = 1;
-        while (Files.exists(destinationFile)) {
-            String newFileName = baseName + "(" + count + ")";
+        while (doesObjectExists(bucketName, s3Key)) {
+           String newFileName = baseName + "(" + count + ")";
             if (!extension.isEmpty()) {
                 newFileName += "." + extension;
             }
-            destinationFile = userFolder.resolve(newFileName).normalize().toAbsolutePath();
-            count += 1;
+            s3Key = userId + "/" + newFileName;
+            count++;
         }
-
+        System.out.println("path = " + s3Key);
+        byte[] imageDataCpy = null;
         try {
             // Vérifie le type de fichier et applique la compression adaptée
             if (extension.equalsIgnoreCase("jpg") || extension.equalsIgnoreCase("jpeg")) {
-                FileCompressService.compressImage(multipartFile, destinationFile);
+                byte[] imageData = FileCompressService.compressImage(multipartFile, destinationFile);
+                imageDataCpy = imageData;
+                PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(s3Key) 
+                    .contentType(extension) 
+                    .build();
+                s3Client.putObject(request, RequestBody.fromBytes(imageData));
+                
+
             } else if (extension.equalsIgnoreCase("txt") || extension.equalsIgnoreCase("csv")){
                 if (multipartFile.getSize() > 2000) {
-                    FileCompressService.compressTxtCsv(multipartFile, destinationFile);
+                        byte[] txtCsvByteArray = FileCompressService.compressTxtCsv(multipartFile, destinationFile);
+                        imageDataCpy = txtCsvByteArray;
+                    PutObjectRequest request = PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(s3Key) 
+                        .contentType(extension) 
+                        .build();
+                    s3Client.putObject(request, RequestBody.fromBytes(txtCsvByteArray));
                 } else {
-                    multipartFile.transferTo(destinationFile);
+                    byte[] rawBytes = multipartFile.getBytes();
+                    imageDataCpy = rawBytes;
+                    PutObjectRequest request = PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(s3Key) 
+                        .contentType(extension) 
+                        .build();
+                    s3Client.putObject(request, RequestBody.fromBytes(rawBytes)); 
                 }
             } else if (extension.equalsIgnoreCase("pdf")) {
-                FileCompressService.compressPDF(multipartFile, destinationFile, 0.3f);
+                byte [] pdfByteArray = FileCompressService.compressPDF(multipartFile, destinationFile, 0.3f);
+                imageDataCpy = pdfByteArray;
+                PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(s3Key) 
+                    .contentType(extension) 
+                    .build();
+                s3Client.putObject(request, RequestBody.fromBytes(pdfByteArray));
             } else if (extension.equalsIgnoreCase("mp3")) {
-                FileCompressService.compressMp3(multipartFile, destinationFile);
+                byte[] mp3ByteArray = FileCompressService.compressMp3(multipartFile, destinationFile);
+                imageDataCpy = mp3ByteArray;
+                PutObjectRequest request = PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(s3Key) 
+                        .contentType("audio/mpeg") 
+                        .build();
+                s3Client.putObject(request, RequestBody.fromBytes(mp3ByteArray));
             } else if (extension.equalsIgnoreCase("mp4")) {
-                FileCompressService.compressMp4(multipartFile, destinationFile);
+               byte[] mp4ByteArray = FileCompressService.compressMp4(multipartFile, destinationFile);
+                imageDataCpy = mp4ByteArray;
+                PutObjectRequest request = PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(s3Key) 
+                        .contentType("video/mp4") 
+                        .build();
+                s3Client.putObject(request, RequestBody.fromBytes(mp4ByteArray));
             } else {
-                multipartFile.transferTo(destinationFile);
+                byte[] fileBytes = multipartFile.getBytes();
+                imageDataCpy = fileBytes;
+                PutObjectRequest request = PutObjectRequest.builder()
+                        .bucket(bucketName)
+                        .key(s3Key) 
+                        .contentType(extension) 
+                        .build();
+                s3Client.putObject(request, RequestBody.fromBytes(fileBytes));
             }
 
             System.out.println("Fichier transféré : " + destinationFile.toString());
 
             File fileEntity = new File();
+            long fileSize = 0;
+            if (Files.exists(destinationFile)) {
+                fileSize = Files.size(destinationFile);
+            } else {
+                fileSize = imageDataCpy.length; // ou multipartFile.getSize() si tu as pas imageData
+            }
             fileEntity.setName(destinationFile.getFileName().toString());
             fileEntity.setPath(destinationFile.toString());
-            fileEntity.setSize(Files.size(destinationFile));
+            fileEntity.setSize(fileSize);
             fileEntity.setFormat(multipartFile.getContentType());
             fileEntity.setUserId(userId);
 
@@ -107,4 +174,18 @@ public class FileServiceImplementation implements FileFacade {
             throw new IllegalArgumentException("Échec du traitement du fichier : " + multipartFile.getOriginalFilename(), e);
         }
     }
+    
+    private boolean doesObjectExists(String bucketName, String key) {
+    try {
+        s3Client.headObject(b -> b.bucket(bucketName).key(key));
+        return true; // L’objet existe
+    } catch (NoSuchKeyException e) {
+        return false; // L’objet n’existe pas
+    } catch (S3Exception e) {
+        if (e.statusCode() == 404) {
+            return false;
+        }
+        throw e; // autre erreur
+    }
+}
 }
